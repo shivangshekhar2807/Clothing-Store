8b1e24a6a092928e5b75d1579c81897d4f4b0f88
shivangshekhar2807 <shivangshekhar302@gmail.com>
2025-03-20 23:39:41 +0530

    updated the input field when stock is over using useRef hook

--- src/components/Clothes/InputClothes.js
+++ src/components/Clothes/InputClothes.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import "./InputClothes.css";
 import AllContext from "../Store/store-context";
 
@@ -7,7 +7,11 @@ function InputClothes({ item }) {
 
     const [valueforS, setValueForS] = useState(item.S);
     const [valueforM, setValueForM] = useState(item.M);
-     const [valueforL, setValueForL] = useState(item.L);
+    const [valueforL, setValueForL] = useState(item.L);
+    
+    const refForS = useRef();
+    const refForM = useRef();
+    const refForL = useRef();
 
     const contextFunction = useContext(AllContext);
     
@@ -17,19 +21,26 @@ function InputClothes({ item }) {
          const MValue = Number(event.target.elements[`M-${item.id}`].value.trim());
         const LValue = Number(event.target.elements[`L-${item.id}`].value.trim());
 
+
         if (valueforS <= 0) {
-            return;
+             refForS.current.value = 0;
+            refForS.current.disabled = true;
+
         }
-         setValueForS(valueforS - SValue);
+         
          
         if (valueforM <= 0) {
-            return;
+            refForM.current.value = 0;
+            refForM.current.disabled = true;
          }
-        setValueForM(valueforM - MValue);
+        
 
         if (valueforL <= 0) {
-            return;
+            refForL.current.value = 0;
+            refForL.current.disabled = true;
         }
+        setValueForS(valueforS - SValue);
+        setValueForM(valueforM - MValue);
         setValueForL(valueforL - LValue);
         
         if (SValue<=0 && MValue<=0 && LValue<=0) {
@@ -56,6 +67,7 @@ function InputClothes({ item }) {
       <div className="input-group">
                 <label htmlFor={`S-${item.id}`}>S:{valueforS}</label>
         <input
+          ref={refForS}
           id={`S-${item.id}`}
           name={`S-${item.id}`}
           type="number"
@@ -68,6 +80,7 @@ function InputClothes({ item }) {
       <div className="input-group">
                 <label htmlFor={`M-${item.id}`}>M:{valueforM}</label>
         <input
+          ref={refForM}
           id={`M-${item.id}`}
           name={`M-${item.id}`}
           type="number"
@@ -80,6 +93,7 @@ function InputClothes({ item }) {
       <div className="input-group">
                 <label htmlFor={`L-${item.id}`}>L:{valueforL}</label>
         <input
+           ref={refForL}
           id={`L-${item.id}`}
           name={`L-${item.id}`}
           type="number"
